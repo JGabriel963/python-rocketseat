f9841dccabf3449ba6f06143940b9151d17e0275
João Gabriel <joaogabriel9633@gmail.com>
2025-08-30 23:49:36 -0300

    Create and Read with Flask

---
+++ flask/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, jsonify
+from models.task import Task
+
+app = Flask(__name__)
+
+tasks = []
+task_id_control = 1
+
+@app.route("/tasks", methods=["POST"])
+def create_task():
+    global task_id_control
+    data = request.get_json()
+    new_task = Task(id=task_id_control, title=data["title"], description=data.get("description", ""))
+    task_id_control += 1
+    tasks.append(new_task)
+    return jsonify({"message": "Nova tarefa criada com sucesso"})
+
+@app.route("/tasks", methods=["GET"])
+def get_tasks():
+    task_list = [task.to_dict() for task in tasks]
+    
+    output = {
+            "tasks": task_list,
+            "total_tasks": 0
+        }
+
+
+    return jsonify(output)
+
+@app.route("/tasks/<int:id>", methods=["GET"])
+def get_task_by_id(id):
+    task = next((task for task in tasks if task.id == id), None)
+    if task:
+        return jsonify(task.to_dict())
+    else:
+        return jsonify({"message": "Tarefa não encontrada"}), 400
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
