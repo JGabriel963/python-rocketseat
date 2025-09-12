4553a996485740fb8e11418c2465095d7f223e06
João Gabriel Lenovo <joaogabriel9633@gmail.com>
2025-09-12 14:00:57 -0300

    Setup login

--- flask-avançado/app.py
+++ flask-avançado/app.py
@@ -1,12 +1,34 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from models.user import User
 from database import db
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
+login_manager = LoginManager()
 db.init_app(app)
+login_manager.init_app(app)
+
+# View login
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        # Login
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+            return jsonify({"message": "Credenciais inválidas"}), 400
+        
+        return jsonify({"message": "Login bem-sucedido"}), 200
+
+    return jsonify({"message": "Credenciais inválidas"}), 400
 
 app.route("/hello-world", methods=["GET"])
 def hello_world():
