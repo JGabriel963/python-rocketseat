aabefd9fdb3bf1d3aa4ba013e8ae7d537c7d093b
João Gabriel <joaogabriel9633@gmail.com>
2026-05-26 14:16:51 -0300

    feat: enhance calculator functionality with new endpoints and integration tests
    - Added POST endpoints for Calculator2 and Calculator3 in the routes.
    - Updated Calculator2 to use a driver handler interface for standard derivation.
    - Implemented integration tests for Calculator2 with mock driver handler.
    - Enhanced numpy handler to implement variance calculation.

--- design_de_codigo/src/main/routes/calculators.py
+++ design_de_codigo/src/main/routes/calculators.py
@@ -1,10 +1,28 @@
 from flask import Blueprint, jsonify, request
-from src.calculators.calculator_1 import Calculator1
+from src.main.factories.calculator1_factory import calculator_1_factory
+from src.main.factories.calculator2_factory import calculator_2_factory
+from src.main.factories.calculator3_factory import calculator_3_factory
 
 calc_routes_bg = Blueprint("cal_routes", __name__)
 
 @calc_routes_bg.route("/calculator/1", methods=["POST"])
 def calculator_1():
-    cal = Calculator1()
+    cal = calculator_1_factory()
     response = cal.calculate(request)
+    
     return jsonify(response), 200
+
+@calc_routes_bg.route("/calculator/2", methods=["POST"])
+def calculator_2():
+    cal = calculator_2_factory()
+    response = cal.calculate(request)
+
+    return jsonify(response), 200
+
+@calc_routes_bg.route("/calculator/3", methods=["POST"])
+def calculator_3():
+    cal = calculator_3_factory()
+    response = cal.calculate(request)
+
+    return jsonify(response), 200
+
