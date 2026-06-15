c33bcee335f2626ab3c1416a39147e1b4e0937f8
João Gabriel <joaogabriel9633@gmail.com>
2026-06-15 14:13:45 -0300

    Implement jwt handler

--- auth-jwt/src/models/repositories/user_repository.py
+++ auth-jwt/src/models/repositories/user_repository.py
@@ -1,38 +1,44 @@
 from sqlite3 import Connection
 
-class UserRepository:
+from src.models.interfaces.user_repository import UserRepositoryInterface
+
+
+class UserRepository(UserRepositoryInterface):
     def __init__(self, conn: Connection) -> None:
         self.__conn = conn
 
     def registry_user(self, username: str, password: str) -> None:
         cursor = self.__conn.cursor()
         cursor.execute(
-            '''
-            INSERT INTO users (username, password, balance) 
+            """
+            INSERT INTO users (username, password, balance)
             VALUES (?, ?, ?);
-            ''', (username, password, 0)
+            """,
+            (username, password, 0),
         )
         self.__conn.commit()
 
     def edit_balance(self, user_id: int, new_balance: float) -> None:
         cursor = self.__conn.cursor()
         cursor.execute(
-            '''
+            """
             UPDATE users
             SET balance = ?
             WHERE id = ?
-            ''', (new_balance, user_id)
+            """,
+            (new_balance, user_id),
         )
         self.__conn.commit()
 
-    def get_user_by_username(self, username: str):
+    def get_user_by_username(self, username: str) -> tuple[int, str, str]:
         cursor = self.__conn.cursor()
         cursor.execute(
-            '''
+            """
             SELECT id, username, password, balance
             FROM users
             WHERE username = ?
-            ''', (username,)
+            """,
+            (username,),
         )
         user = cursor.fetchone()
         return user
